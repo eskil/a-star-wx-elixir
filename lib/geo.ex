5c51c30fba3db72a9f1ec82e34a5cba5872a1878
Eskil Heyn Olsen <eskil@eskil.org>
2023-03-08 14:30:02 -0800

    Tests and docs

--- lib/geo.ex
+++ lib/geo.ex
@@ -60,8 +60,8 @@ defmodule Geo do
   `[]` if there's no intersections.
 
   ## Examples
-      iex> polygon = [{0, 0}, {2, 0}, {2, 1}, {1, 0.5}, {0, 1}]
-      [{0, 0}, {2, 0}, {2, 1}, {1, 0.5}, {0, 1}]
+      iex> polygon = [{0, 0}, {0, 1}, {1, 0.5}, {2, 1}, {2, 0}]
+      [{0, 0}, {0, 1}, {1, 0.5}, {2, 1}, {2, 0}]
       iex> line = {{1, -1}, {1, 3}}
       {{1, -1}, {1, 3}}
       iex> Geo.intersections(line, polygon)
